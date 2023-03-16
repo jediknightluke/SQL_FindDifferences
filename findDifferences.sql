a2a09d63bd204a08525251b65afcf7757e650abc
jediknightluke <luke.lukemelton.melton@gmail.com>
2023-03-16 11:38:09 -0400

    Update findDifferences.sql
    Updated for converting to sp

--- findDifferences.sql
+++ findDifferences.sql
@@ -1,78 +1,56 @@
--- Set the database names
-USE first_database;
-DECLARE @first_database_name AS VARCHAR(50) = DB_NAME();
-
-USE second_database;
-DECLARE @second_database_name AS VARCHAR(50) = DB_NAME();
-
--- Create temporary tables to store the results
-CREATE TABLE #differences (table_name VARCHAR(100), column_name VARCHAR(100), difference VARCHAR(100));
-
--- Find the tables that exist in both databases
-SELECT TABLE_NAME
-INTO #common_tables
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE'
-AND TABLE_NAME IN (
-    SELECT TABLE_NAME
-    FROM first_database.INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_TYPE = 'BASE TABLE'
-    AND TABLE_NAME IN (
-        SELECT TABLE_NAME
-        FROM second_database.INFORMATION_SCHEMA.TABLES
-        WHERE TABLE_TYPE = 'BASE TABLE'
-    )
-);
-
--- Loop through the common tables and compare the columns
-DECLARE @table_name AS VARCHAR(100);
-
-DECLARE common_tables_cursor CURSOR FOR
-SELECT table_name FROM #common_tables;
-
-OPEN common_tables_cursor;
-
-FETCH NEXT FROM common_tables_cursor
-INTO @table_name;
-
-WHILE @@FETCH_STATUS = 0
+CREATE PROCEDURE CompareDatabases
+    @Database1 nvarchar(50),
+    @Database2 nvarchar(50)
+AS
 BEGIN
-    INSERT INTO #differences
-    SELECT
-        @table_name,
-        c1.COLUMN_NAME,
-        'Data Type Mismatch'
-    FROM first_database.INFORMATION_SCHEMA.COLUMNS c1
-    LEFT JOIN second_database.INFORMATION_SCHEMA.COLUMNS c2
-    ON c1.TABLE_NAME = c2.TABLE_NAME
-    AND c1.COLUMN_NAME = c2.COLUMN_NAME
-    WHERE c1.TABLE_NAME = @table_name
-    AND c2.COLUMN_NAME IS NULL
-    AND c1.DATA_TYPE <> 'timestamp';
+    SET NOCOUNT ON;
 
-    INSERT INTO #differences
-    SELECT
-        @table_name,
-        c2.COLUMN_NAME,
-        'Data Type Mismatch'
-    FROM first_database.INFORMATION_SCHEMA.COLUMNS c1
-    RIGHT JOIN second_database.INFORMATION_SCHEMA.COLUMNS c2
-    ON c1.TABLE_NAME = c2.TABLE_NAME
-    AND c1.COLUMN_NAME = c2.COLUMN_NAME
-    WHERE c2.TABLE_NAME = @table_name
-    AND c1.COLUMN_NAME IS NULL
-    AND c2.DATA_TYPE <> 'timestamp';
+    -- Compare tables
+    SELECT @Database1 AS DatabaseName,
+           t1.name AS TableName,
+           'Table' AS ObjectType,
+           'Missing in ' + @Database2 AS DifferenceType
+    FROM sys.tables t1
+    WHERE NOT EXISTS (SELECT 1 FROM sys.tables t2 WHERE t2.name = t1.name AND t2.type = 'U' AND t2.object_id = OBJECT_ID(@Database2 + '.dbo.' + t1.name))
+        AND t1.type = 'U'
+    UNION ALL
+    SELECT @Database2 AS DatabaseName,
+           t2.name AS TableName,
+           'Table' AS ObjectType,
+           'Missing in ' + @Database1 AS DifferenceType
+    FROM sys.tables t2
+    WHERE NOT EXISTS (SELECT 1 FROM sys.tables t1 WHERE t1.name = t2.name AND t1.type = 'U' AND t1.object_id = OBJECT_ID(@Database1 + '.dbo.' + t2.name))
+        AND t2.type = 'U'
 
-    FETCH NEXT FROM common_tables_cursor
-    INTO @table_name;
-END;
+    -- Compare views
+    UNION ALL
+    SELECT @Database1 AS DatabaseName,
+           v1.name AS ViewName,
+           'View' AS ObjectType,
+           'Missing in ' + @Database2 AS DifferenceType
+    FROM sys.views v1
+    WHERE NOT EXISTS (SELECT 1 FROM sys.views v2 WHERE v2.name = v1.name AND v2.object_id = OBJECT_ID(@Database2 + '.dbo.' + v1.name))
+    UNION ALL
+    SELECT @Database2 AS DatabaseName,
+           v2.name AS ViewName,
+           'View' AS ObjectType,
+           'Missing in ' + @Database1 AS DifferenceType
+    FROM sys.views v2
+    WHERE NOT EXISTS (SELECT 1 FROM sys.views v1 WHERE v1.name = v2.name AND v1.object_id = OBJECT_ID(@Database1 + '.dbo.' + v2.name))
 
-CLOSE common_tables_cursor;
-DEALLOCATE common_tables_cursor;
-
--- Select the results
-SELECT * FROM #differences;
-
--- Clean up the temporary tables
-DROP TABLE #common_tables;
-DROP TABLE #differences;
+    -- Compare stored procedures
+    UNION ALL
+    SELECT @Database1 AS DatabaseName,
+           sp1.name AS ProcedureName,
+           'Stored Procedure' AS ObjectType,
+           'Missing in ' + @Database2 AS DifferenceType
+    FROM sys.procedures sp1
+    WHERE NOT EXISTS (SELECT 1 FROM sys.procedures sp2 WHERE sp2.name = sp1.name AND sp2.object_id = OBJECT_ID(@Database2 + '.dbo.' + sp1.name))
+    UNION ALL
+    SELECT @Database2 AS DatabaseName,
+           sp2.name AS ProcedureName,
+           'Stored Procedure' AS ObjectType,
+           'Missing in ' + @Database1 AS DifferenceType
+    FROM sys.procedures sp2
+    WHERE NOT EXISTS (SELECT 1 FROM sys.procedures sp1 WHERE sp1.name = sp2.name AND sp1.object_id = OBJECT_ID(@Database1 + '.dbo.' + sp2.name))
+END
